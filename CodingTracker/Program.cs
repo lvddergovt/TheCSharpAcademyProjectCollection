3dff33710f70460821c8103ee21a40dcf6b070d5
Guido <guido@thinksharp.nl>
2024-01-23 14:41:25 +0100

    Add codingtracker project

---
+++ CodingTracker/Program.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console;
+
+namespace CodingTracker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // todo: 
+      // dotnet add package Spectre.Console
+      // dotnet add package Spectre.Console.Cli
+      // dotnet add package Microsoft.EntityFrameworkCore.Sqlite
+
+
+
+      AnsiConsole.Render(new FigletText("Coding Tracker").Centered().Color(Color.Green));
+    }
+  }
+}
