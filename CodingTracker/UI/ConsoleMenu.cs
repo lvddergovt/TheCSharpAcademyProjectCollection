80e7bf814d01302634791b7d6f37dad9554e44fa
Guido Bakkes <lvddergovt@users.noreply.github.com>
2024-01-25 22:25:56 +0100

    Refactor console menu options

--- CodingTracker/UI/ConsoleMenu.cs
+++ CodingTracker/UI/ConsoleMenu.cs
@@ -23,22 +23,14 @@ namespace CodingTracker
                 Console.Clear();
                 var panel = new Panel(new Rows(
                     new Text("1. Add New Coding Session"),
-                    new Text("2. Edit Coding Session"),
-                    new Text("3. Remove Coding Session"),
-                    new Text("4. View All Coding Sessions"),
-                    new Text("5. Exit"))
+                    new Text("2. Remove Coding Session"),
+                    new Text("3. View All Coding Sessions"),
+                    new Text("4. Exit"))
                 );
                 panel.Border = BoxBorder.Double;
                 panel.Padding = new Padding(2, 2, 2, 2);
                 panel.Header = new PanelHeader("Coding Tracker");
                 AnsiConsole.Render(panel);
-                Console.WriteLine("Coding Tracker Application");
-                Console.WriteLine("1. Add New Coding Session");
-                Console.WriteLine("2. Edit Coding Session");
-                Console.WriteLine("3. Remove Coding Session");
-                Console.WriteLine("4. View All Coding Sessions");
-                Console.WriteLine("5. Exit");
-                Console.Write("\nSelect an option: ");
 
                 switch (Console.ReadLine())
                 {
@@ -46,17 +38,14 @@ namespace CodingTracker
                         _codingSessionController.CreateCodingSessionFromUserInput();
                         break;
                     case "2":
-                        // Edit Coding Session
-                        break;
-                    case "3":
                         // Remove Coding Session
                         _codingSessionController.RemoveCodingSession();
                         break;
-                    case "4":
+                    case "3":
                         // View All Coding Sessions
                         _codingSessionController.DisplayAllCodingSessions();
                         break;
-                    case "5":
+                    case "4":
                         exitApp = true;
 
                         AnsiConsole.Render(new FigletText("Take care, happy coding!").Centered().Color(Color.Green));
